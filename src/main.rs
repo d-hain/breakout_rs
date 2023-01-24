d2bbe550742373c7f33398dfb1cc1c94c37d53ef
dhain <d.hain@gmx.at>
2023-01-24 23:58:01 +0100

    Paddle is drawing
    - created Window struct (will very likely be removed)
    
    - created Paddle struct for the player paddle
    
    - added window and paddle to GameState struct
      (window will very likely be removed)
    
    - game is now updating at 60 fps/ups locked
    
    - fps counter is displayed top left
    
    - paddle is being drawn
    
    - basic window setup

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,118 @@
-use ggez::{conf, Context, ContextBuilder, GameError};
+#![warn(clippy::unwrap_used)]
+
+use ggez::*;
+use ggez::conf::{Conf, WindowMode, WindowSetup};
+use ggez::graphics::{Color, DrawParam, Quad, Rect, Text};
+
+#[derive(Copy, Clone)]
+struct Window {
+    width: f32,
+    height: f32,
+    min_width: f32,
+    min_height: f32,
+}
+
+struct Paddle {
+    rect: Rect,
+    speed: i32,
+    // image: Image, // TODO: cannot get Image to load
+}
 
 struct GameState {
     delta_time: std::time::Duration,
+    window: Window,
+    paddle: Paddle,
 }
 
-impl ggez::event::EventHandler<GameError> for GameState {
+impl event::EventHandler<GameError> for GameState {
     fn update(&mut self, context: &mut Context) -> Result<(), GameError> {
-        self.delta_time = context.time.delta();
+        const TARGET_FPS: u8 = 60;
+        
+        while context.time.check_update_time(TARGET_FPS as u32) {
+            self.delta_time = context.time.delta();
+            //TODO: update gameState window height and width in if expression
+        }
         
         Ok(())
     }
 
     fn draw(&mut self, context: &mut Context) -> Result<(), GameError> {
-        let seconds = self.delta_time.as_secs_f32();
-        println!("Hello, breakout! dt={}s", seconds);
-        
+        let mut canvas = graphics::Canvas::from_frame(context, Color::from([0.1, 0.5, 0.3, 1.0]));
+            
+        // TODO: fixed framerate
+        // draw fps counter
+        let fps = context.time.fps();
+        let fps_display = Text::new(format!("FPS: {}", fps));
+        canvas.draw(
+            &fps_display,
+            DrawParam::from([0.0, 0.0]).color(Color::WHITE),
+        );
+
+        // draw paddle
+        let paddle_rect = &self.paddle.rect;
+        canvas.draw(
+            &Quad,
+            DrawParam::new()
+                .dest(paddle_rect.point())
+                .scale(paddle_rect.size())
+                .color(Color::WHITE),
+        );
+
+        canvas.finish(context)?;
         Ok(())
     }
 }
 
-
 fn main() {
-    let state = GameState {
-        delta_time: std::time::Duration::new(0, 0),
-    };
-    let conf = conf::Conf::new();
+    let (conf, window) = setup_conf();
     let (context, event_loop) = ContextBuilder::new("breakout_rs", "DocE")
         .default_conf(conf)
         .build()
-        .unwrap();
-    
-    ggez::event::run(context, event_loop, state);
+        .expect("Building context failed!");
+
+    let game_state = GameState {
+        delta_time: std::time::Duration::new(0, 0),
+        window,
+        paddle: Paddle {
+            rect: Rect::new(
+                window.width / 10.0, 
+                window.height - (window.height / 10.0), 
+                window.width / 10.0, 
+                window.height / 20.0,
+            ),
+            speed: 1,
+            // image: Image::from_path(&context, "/ltt-logo.png").expect("Loading paddle image failed!"),
+            //TODO: cannot get Image to load
+        },
+    };
+
+    event::run(context, event_loop, game_state);
+}
+
+fn setup_conf() -> (Conf, Window) {
+    let window = Window {
+        width: 800.0,
+        height: 600.0,
+        min_width: 800.0,
+        min_height: 600.0,
+    };
+
+    let conf = Conf {
+        window_mode: WindowMode {
+            width: window.width,
+            height: window.height,
+            min_width: window.min_width,
+            min_height: window.min_height,
+            resizable: true,
+            ..WindowMode::default()
+        },
+        window_setup: WindowSetup {
+            title: "breakout_rs".to_string(),
+            icon: "".to_string(),
+            ..WindowSetup::default()
+        },
+        ..Conf::default()
+    };
+
+    (conf, window)
 }
