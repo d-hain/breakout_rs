1d980469fbacd1c53489c6a0d875671553c3e6bf
dhain <d.hain@gmx.at>
2023-01-24 20:16:31 +0100

    Hello, breakout!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use ggez::{conf, Context, ContextBuilder, GameError};
+
+struct GameState {
+    delta_time: std::time::Duration,
+}
+
+impl ggez::event::EventHandler<GameError> for GameState {
+    fn update(&mut self, context: &mut Context) -> Result<(), GameError> {
+        self.delta_time = context.time.delta();
+        
+        Ok(())
+    }
+
+    fn draw(&mut self, context: &mut Context) -> Result<(), GameError> {
+        let seconds = self.delta_time.as_secs_f32();
+        println!("Hello, breakout! dt={}s", seconds);
+        
+        Ok(())
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let state = GameState {
+        delta_time: std::time::Duration::new(0, 0),
+    };
+    let conf = conf::Conf::new();
+    let (context, event_loop) = ContextBuilder::new("breakout_rs", "DocE")
+        .default_conf(conf)
+        .build()
+        .unwrap();
+    
+    ggez::event::run(context, event_loop, state);
 }
